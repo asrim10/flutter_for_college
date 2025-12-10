f7582234be68c041d1a98978849cf8f10aa6799c
asrim10 <asrimmsuwall@gmail.com>
2025-12-10 10:45:29 +0545

    buttom_navigation

--- lib/screens/bottom_navigation_screen.dart
+++ lib/screens/bottom_navigation_screen.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_college/screens/bottom_screen/about_screen.dart';
+import 'package:flutter_for_college/screens/bottom_screen/cart_screen.dart';
+import 'package:flutter_for_college/screens/bottom_screen/home_screen.dart';
+import 'package:flutter_for_college/screens/bottom_screen/profile_screen.dart';
 
-class BottomNavigationScreen extends StatelessWidget {
+class BottomNavigationScreen extends StatefulWidget {
   const BottomNavigationScreen({super.key});
 
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+    const AboutScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dashboard"),
         backgroundColor: Colors.blueGrey,
+      ),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'About'),
+        ],
+        backgroundColor: Colors.tealAccent,
+        selectedItemColor: Colors.pinkAccent,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
